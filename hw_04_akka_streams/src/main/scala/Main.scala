8ef2e53547d14340dccd608bf907e3f46f87d7f6
slow3586 <6337520+slow3586@users.noreply.github.com>
2024-05-04 19:17:03 +0700

    Hw 04: Akka streams 2

--- hw_04_akka_streams/src/main/scala/Main.scala
+++ hw_04_akka_streams/src/main/scala/Main.scala
@@ -6,7 +6,7 @@ import akka.stream.scaladsl.GraphDSL.Implicits._
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, ZipN}
 import com.typesafe.config.ConfigFactory
 import org.apache.kafka.clients.admin.{Admin, AdminClientConfig, NewTopic}
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
@@ -14,7 +14,7 @@ import java.util
 import scala.concurrent.ExecutionContextExecutor
 import scala.language.postfixOps
 
-object Main {
+object Main extends App {
   private val HOST = "kafka"
   private val PORT0 = HOST + ":9091"
   private val PORT1 = HOST + ":9092"
@@ -25,59 +25,69 @@ object Main {
   private val TOPIC = "TOPIC_NAME"
   private val SUBSCRIPTION = Subscriptions.topics(TOPIC)
   private val PRODUCER_SETTINGS: ProducerSettings[String, String] = ProducerSettings(
-    ConfigFactory.load().getConfig("akka.kafka.producer"),
+    SYSTEM,
     new StringSerializer,
     new StringSerializer
   ).withBootstrapServers(PORT0)
   private val CONSUMER_SETTINGS: ConsumerSettings[String, String] = ConsumerSettings(
-    ConfigFactory.load().getConfig("akka.kafka.consumer"),
+    SYSTEM,
     new StringDeserializer,
     new StringDeserializer
   ).withBootstrapServers(PORT1)
     .withGroupId("GROUP_ID")
+    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
   
-  def main(args: Array[String]): Unit = {
-    println("Creating topic!")
-    val admin: Admin = Admin.create(util.Map.of(
-      AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, PORT0.asInstanceOf[Object]))
-    admin.createTopics(util.List.of(new NewTopic(
-      TOPIC,
-      1,
-      1.toShort)))
-    admin.close()
-    println("Topic created!")
-    
-    Thread.sleep(1000)
-    
-    println("Starting consumer!")
-    RunnableGraph.fromGraph(GraphDSL.create() {
-      implicit builder =>
-        val broadcast = builder.add(Broadcast[Int](3))
-        val zip = builder.add(ZipN[Int](3))
-        
-        builder.add(Consumer.plainSource(CONSUMER_SETTINGS, SUBSCRIPTION)) ~>
-          builder.add(Flow[ConsumerRecord[String, String]].map(r => r.value().toInt)) ~>
-          broadcast
-        
-        broadcast.out(0) ~> builder.add(Flow[Int].map(x => x * 10)) ~> zip.in(0)
-        broadcast.out(1) ~> builder.add(Flow[Int].map(x => x * 2)) ~> zip.in(1)
-        broadcast.out(2) ~> builder.add(Flow[Int].map(x => x * 3)) ~> zip.in(2)
-        
-        zip.out ~> builder.add(Sink.foreach(println))
-        
-        ClosedShape
-    }).run
-    
-    Thread.sleep(1000)
-    
-    println("Running producer!")
-    RunnableGraph.fromGraph(GraphDSL.create() {
-      implicit builder =>
-        builder.add(Source(1 to 10)) ~>
-          builder.add(Flow[Int].map(i => new ProducerRecord[String, String](TOPIC, i.toString))) ~>
-          Producer.plainSink(PRODUCER_SETTINGS)
-        
-        ClosedShape
-    }).run
-  }
+  println("Creating topic!")
+  val admin: Admin = Admin.create(util.Map.of(
+    AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, PORT0.asInstanceOf[Object]))
+  admin.createTopics(util.List.of(new NewTopic(
+    TOPIC,
+    1,
+    1.toShort)))
+  admin.close()
+  println("Topic created!")
+  
+  Thread.sleep(1000)
+  
+  println("Starting consumer!")
+  RunnableGraph.fromGraph(GraphDSL.create() {
+    implicit builder =>
+      val input = builder.add(Consumer.plainSource(CONSUMER_SETTINGS, SUBSCRIPTION))
+      val recordMap = builder.add(Flow[ConsumerRecord[String, String]].map(r => r.value().toInt))
+      
+      val broadcast = builder.add(Broadcast[Int](3))
+      val zip = builder.add(ZipN[Int](3))
+      
+      val map0 = builder.add(Flow[Int].map(x => x * 10))
+      val map1 = builder.add(Flow[Int].map(x => x * 2))
+      val map2 = builder.add(Flow[Int].map(x => x * 3))
+      
+      val sum = builder.add(Flow[Seq[Int]].map(v => v.sum))
+      
+      val sink = builder.add(Sink.foreach(println))
+      
+      input ~> recordMap ~> broadcast
+      
+      broadcast.out(0) ~> map0 ~> zip.in(0)
+      broadcast.out(1) ~> map1 ~> zip.in(1)
+      broadcast.out(2) ~> map2 ~> zip.in(2)
+      
+      zip.out ~> sum ~> sink
+      
+      ClosedShape
+  }).run
+  
+  Thread.sleep(1000)
+  
+  println("Running producer!")
+  RunnableGraph.fromGraph(GraphDSL.create() {
+    implicit builder =>
+      builder.add(Source(1 to 15)) ~>
+        builder.add(Flow[Int].map(i => new ProducerRecord[String, String](TOPIC, i.toString))) ~>
+        Producer.plainSink(PRODUCER_SETTINGS)
+      
+      ClosedShape
+  }).run
+  
+  Thread.sleep(120_000)
 }
